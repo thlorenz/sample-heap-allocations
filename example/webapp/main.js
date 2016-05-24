c0b1b3cf82fd596f804f27f696b3b6a0527eb1a5
Thorsten Lorenz <thlorenz@gmx.de>
2016-05-24 16:00:36 -0700

    initial webapp without source

---
+++ example/webapp/main.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const xhr = require('xhr')
+const controlPanelLinks = document.querySelectorAll('.control-panel a')
+const messagesEl = document.getElementById('messages')
+const samplesEl = document.getElementById('samples')
+
+function logResponse(res) {
+  var html = '<span><em>' + res.status + '</em></span>&nbsp;'
+  if (res.error) {
+    html += '<span class="error">' + res.error + '</span>'
+  } else {
+    html += '<span class="message">' + res.msg + '</spanp>'
+  }
+  messagesEl.innerHTML = html
+}
+
+function formattedOnly(data) {
+  function stringify(name, x) {
+    return name + ' ' + x.location + ' --- (' + x.allocations.join(' | ') + ')'
+  }
+  return { callsites: data.callsites.map(x => stringify(x.name, x.formatted)) }
+}
+
+function renderData(data) {
+  /*global JsonHuman */
+  const formatted = data.map(formattedOnly)
+  const node = JsonHuman.format(formatted)
+  samplesEl.appendChild(node)
+}
+
+function onresponse(err, res) {
+  if (err) {
+    console.error(err)
+    return logResponse({ error: err, status: 500 })
+  }
+  var data = JSON.parse(res.body)
+  data.status = res.statusCode
+  if (data.data) renderData(data.data)
+  if (data.type === 'message') return logResponse(data)
+}
+
+function processRequest(href) {
+  xhr({ uri: href }, onresponse)
+}
+
+function hookLink(a) {
+  function onclick(e) {
+    e.preventDefault()
+    processRequest(a.getAttribute('href'))
+    return false
+  }
+  a.onclick = onclick
+}
+
+for (var i = 0; i < controlPanelLinks.length; i++) {
+  hookLink(controlPanelLinks[i])
+}
---
+++ example/webapp/server.js
@@ -0,0 +1,108 @@
+'use strict'
+
+const PORT = 8000
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+const build = require('./build')
+
+const sh = require('../../')
+const allocate = require('../allocate')
+
+const server = http.createServer()
+
+server
+  .on('request', onRequest)
+  .on('listening', onListening)
+  .listen(PORT)
+
+process.on('SIGTERM', onSIGTERM)
+
+function onSIGTERM() {
+  console.error('Caught SIGTERM, shutting down.')
+  server.close()
+  process.exit(0)
+}
+
+console.error('pid', process.pid)
+
+function sendError(res, err) {
+  res.end(JSON.stringify({ error: err.message }))
+}
+
+function serveIndex(res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res)
+}
+
+function serveBundle(res) {
+  res.writeHead(200, { 'Content-Type': 'application/javascript' })
+  build().pipe(res)
+}
+
+function serveCss(res) {
+  res.writeHead(200, { 'Content-Type': 'text/css' })
+  fs.createReadStream(path.join(__dirname, 'index.css')).pipe(res)
+}
+
+function serveDep(res, p) {
+  res.writeHead(200, { 'Content-Type': 'text/css' })
+  fs.createReadStream(path.join(__dirname, 'deps', p)).pipe(res)
+}
+
+function serveStartSampling(res) {
+  sh.startSampling()
+  const json = JSON.stringify({ type: 'message', msg: 'Sampling Started' })
+  res.writeHead(200, { 'Content-Type': 'application/json', 'Content-Length': json.length })
+  res.end(json)
+}
+
+function serveStopSampling(res) {
+  // important to collect allocations before stopping to sample
+  const allocs = sh.collectAllocations()
+  sh.stopSampling()
+  // need to ignore core files before we can add in source code
+  sh.addFormat({ allocations: allocs, includeSourceLines: 0 }, onformatted)
+
+  function onformatted(err) {
+    if (err) return sendError(res, err)
+    const json = JSON.stringify({ type: 'message', msg: 'Sampling Stopped', data: allocs })
+    res.writeHead(200, { 'Content-Type': 'application/json', 'Content-Length': json.length })
+    res.end(json)
+  }
+}
+
+function serveAllocateFlat(res) {
+  allocate.flatArray()
+  const json = JSON.stringify({ type: 'message', msg: 'Flat Array Allocated' })
+  res.writeHead(200, { 'Content-Type': 'application/json', 'Content-Length': json.length })
+  res.end(json)
+}
+
+function serveAllocateNested(res) {
+  allocate.innerOuterArray()
+  const json = JSON.stringify({ type: 'message', msg: 'Nested Array Allocated' })
+  res.writeHead(200, { 'Content-Type': 'application/json', 'Content-Length': json.length })
+  res.end(json)
+}
+
+// Handle Requests
+function onRequest(req, res) {
+  console.error('%s %s', req.method, req.url)
+
+  if (req.url.startsWith('/deps')) return serveDep(res, req.url.slice('/deps/'.length))
+  if (req.url === '/') return serveIndex(res)
+  if (req.url === '/bundle.js') return serveBundle(res)
+  if (req.url === '/index.css') return serveCss(res)
+
+  if (req.url === '/start_sampling') return serveStartSampling(res)
+  if (req.url === '/allocate_flat') return serveAllocateFlat(res)
+  if (req.url === '/allocate_nested') return serveAllocateNested(res)
+  if (req.url === '/stop_sampling') return serveStopSampling(res)
+
+  sendError(res, new Error('Page not found ' + req.url))
+}
+
+function onListening() {
+  console.error('HTTP server listening on http://localhost:%d', this.address().port)
+}
