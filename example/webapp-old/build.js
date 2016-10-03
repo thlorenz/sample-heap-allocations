73cc4149916e32e665706f66ce9c4701827e8981
Thorsten Lorenz <thlorenz@gmx.de>
2016-10-03 11:10:00 -0500

    sample: removing olde webapp

--- example/webapp-old/build.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var browserify = require('browserify')
-
-module.exports = function build() {
-  return browserify({ debug: true })
-    .require(require.resolve('./main'), { entry: true })
-    .bundle()
-}
